60bdfb6bd719c7693719c8385624d25af6a5327c
Gopi <Gopi@LAPTOP-I8HCUJBT>
2025-08-05 21:43:30 +0530

    Sum of 2numbers

---
+++ KPRIT/src/Sumofnum.java
@@ -0,0 +1,13 @@
+//sum of numbers
+public class Sumofnum {
+	
+	int a=6;
+	int b=9;
+	
+	int c = a + b;
+	
+	void sum() {
+		System.out.println(c);
+	}
+	
+}
---
+++ KPRIT/src/SumofnumMain.java
@@ -0,0 +1,10 @@
+
+public class SumofnumMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sumofnum num = new Sumofnum();
+		num.sum();
+	}
+
+}
