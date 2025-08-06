e8279172ac3e51d2f7b61791feb28421dda116ee
gopieee06 <gopianem424@gmail.com>
2025-08-06 19:15:09 +0530

    Largest num

---
+++ KPRIT/src/LargestNum.java
@@ -0,0 +1,18 @@
+
+public class LargestNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=34,b=26,c=76;
+		if (a>=b && a>=c) {
+			System.out.println("A is Largest");
+		}
+		else if (b>=c && b>=a) {
+			System.out.println("B is Largest");
+		}
+		else {
+			System.out.println("C is Largest");
+		}
+	}
+
+}
