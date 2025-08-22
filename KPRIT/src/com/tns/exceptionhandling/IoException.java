f41d6b1d5a887e0cd7ad7ddddf2bc66b55fffec6
gopieee06 <gopianem424@gmail.com>
2025-08-22 21:42:42 +0530

    Exception Handling Demo

---
+++ KPRIT/src/com/tns/exceptionhandling/IoException.java
@@ -0,0 +1,32 @@
+package com.tns.exceptionhandling;
+//Demo for Wrong input Exception Handling Demo
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class IoException {
+	public static void main(String args[]) {
+		int num,sum=0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a number : ");
+		
+		try {
+			num = sc.nextInt();
+			for(int i=1;i<=num;i++) {
+				sum = sum + i;
+			}
+			System.out.println("Sum of " + num + " numbers is : " + sum);
+		}
+		catch (InputMismatchException Io) {
+//			System.out.println("You Enter String/Charter intead of integer Values.");
+			Io.printStackTrace();
+		}
+		finally {
+			System.out.println("Input Tacken Successfully.");
+		}
+		
+
+	}
+}
